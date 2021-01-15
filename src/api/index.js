25f63ffe4e5d243d05f522996a89979bd237968c
bluese0712 <593159158@qq.com>
2021-01-15 10:09:59 +0800

    投资基金管理初步实现

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import cwRecord from './finance/record';
 import cwCategory from './finance/category';
+import cwFund from './finance/fund';
 
 export default {
     cwRecord,
-    cwCategory
+    cwCategory,
+    cwFund
 };
